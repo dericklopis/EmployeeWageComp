3827ba9b813c252e1d63d8c9a336a1b0740ace61
dericklopis <stanforever08@gmail.com>
2022-11-14 21:16:04 +0530

    Added:EmployeeHours

--- EmployeeWageComp/EmployeeWage.cs
+++ EmployeeWageComp/EmployeeWage.cs
@@ -12,19 +12,25 @@ namespace EmployeeWageComp
         {
             //Constants
             int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
+            //Variables 
+            int empHrs = 0;
+            int empWage = 0;
             Random random = new Random();
             //Computation
             int empCheck = random.Next(0, 2);
             if (empCheck == IS_FULL_TIME)
             {
-                Console.WriteLine("Employee is Present");
-                Console.ReadLine();
+                empHrs = 8;
             }
             else
             {
-                Console.WriteLine("Employee is Absent");
-                Console.ReadLine();
+                empHrs = 0;
             }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage : " + empWage);
+            Console.ReadLine();
+            
         }
     }
 }
