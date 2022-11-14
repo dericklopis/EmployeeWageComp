2aa9dedd0a240f3388ec22d67c3736cfd52aec2c
dericklopis <stanforever08@gmail.com>
2022-11-14 21:42:41 +0530

    Added:EmployeeForLoopUC5

--- EmployeeWageComp/EmployeeWage.cs
+++ EmployeeWageComp/EmployeeWage.cs
@@ -11,28 +11,33 @@ namespace EmployeeWageComp
         public const int IS_PART_TIME = 1;
         public const int IS_FULL_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
+        public const int NUM_OF_WORKING_DAYS = 20;
         static void Main(string[] args)
         {
             //Variables
-            int empHrs = 0;
-            int empWage = 0;
-            Random random = new Random();
+            int empHrs = 0, empWage = 0, totalEmpWage = 0;
             //Computation
-            int empCheck = random.Next(0, 3);
-            switch (empCheck)
-            {
-                case IS_PART_TIME:
-                    empHrs = 4;
-                    break;
-                case IS_FULL_TIME:
-                    empHrs = 8;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
+            for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
+     {
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+                totalEmpWage += empWage;
+                Console.WriteLine("Emp Wage : " + empWage);
             }
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Emp Wage : " + empWage);
+            Console.WriteLine("Total Emp Wage :" + totalEmpWage);
             Console.ReadLine();
         }
     }
