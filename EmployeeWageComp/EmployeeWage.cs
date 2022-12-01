0551dcc60b781c5af820904ed8504b0498d62cc5
dericklopis <stanforever08@gmail.com>
2022-12-01 16:57:12 +0530

    Added: Refactor the code

--- EmployeeWageComp/EmployeeWage.cs
+++ EmployeeWageComp/EmployeeWage.cs
@@ -11,14 +11,14 @@ namespace EmployeeWageComp
         public const int IS_PART_TIME = 1;
         public const int IS_FULL_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
-        public const int NUM_OF_WORKING_DAYS = 20;
+        public const int NUM_OF_WORKING_DAYS = 2;
         public const int MAX_HRS_IN_MONTH = 10;
-        static void Main(string[] args)
+        public static int computeEmpWage()
         {
             //Variables
             int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
             //Computation
-            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
+            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays<NUM_OF_WORKING_DAYS)
             {
                 totalWorkingDays++;
                 Random random = new Random();
@@ -26,7 +26,7 @@ namespace EmployeeWageComp
                 switch (empCheck)
                 {
                     case IS_PART_TIME:
-                        empHrs = 4;
+                        empHrs =4;
                         break;
                     case IS_FULL_TIME:
                         empHrs = 8;
@@ -36,10 +36,15 @@ namespace EmployeeWageComp
                         break;
                 }
                 totalEmpHrs += empHrs;
-                Console.WriteLine("Day#:" + totalWorkingDays + " Emp Hrs: " + empHrs);
+                Console.WriteLine("Day#:" + totalWorkingDays + " Emp Hrs : " + empHrs);
             }
             int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Total Emp Wage : "+ totalEmpWage);
+            Console.WriteLine("Total Emp Wage: " + totalEmpWage);
+            return totalEmpWage;
+        }
+        static void Main(string[] args)
+        {
+            computeEmpWage();
             Console.ReadLine();
         }
     }
